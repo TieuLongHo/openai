992b356a7ac102985bd9b138166e1336f7053320
Tieu Long <tieu.ho@students.bfh.ch>
2023-04-07 02:51:47 +0200

    add gitignore

---
+++ README.md
@@ -0,0 +1,36 @@
+# ChatGPT and DALL-E CLI
+
+This Python program uses two different AI models - GPT and Dalle API - to generate text and images respectively. GPT is a text-generating AI model and Dalle is an image-generating AI model. The program can be used to generate text and images based on user input.
+
+## Requirements
+
+To run this program, you will need the following:
+
+- Python 3.7 or higher
+- OpenAI API Key for GPT and DALL-E(Available at https://beta.openai.com/signup/)
+
+## Installation
+
+To get started with this program, follow the steps below:
+
+1. Clone the repository to your local machine.
+2. Run `pip install openai`
+3. Add your API key as an envirement variable `OPENAI_API_KEY`
+
+## Usage
+
+To use this program, follow the steps below:
+
+1. Open a terminal window and navigate to the directory where you have cloned the repository.
+2. Run the command `python openai.py`.
+3. Follow the prompts to provide input for GPT and Dalle models to generate text and image respectively.
+
+## Credits
+
+This program was created by Tieu Long Ho as part of an fun midnight test project.
+
+It uses OpenAI's GPT API and Dalle API.
+
+## License
+
+This program is licensed under the [MIT License](https://opensource.org/licenses/MIT)
---
+++ openai.py
@@ -0,0 +1,80 @@
+import os
+import openai
+from termcolor import colored
+import argparse
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+history = [
+    {"role": "system", "content": "You are a helpful assistant."}
+]
+
+
+def generate_chat_response(user_input):
+    history.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=history
+    )
+    history.append(
+        {"role": "system", "content": response.choices[0].message.content})
+    return response
+
+
+def generate_image_response(user_input):
+    response = response = openai.Image.create(
+        prompt=user_input,
+        n=3,
+        size="1024x1024"
+    )
+
+    return response
+
+
+def chat(debug):
+    while True:
+        user_input = input('you: ')
+        if user_input.lower() in ['quit', 'exit']:
+            break
+        response = generate_chat_response(user_input)
+        print("gpt:", colored(
+            response['choices'][0]['message']['content'], 'white', "on_dark_grey"))
+        if debug:
+            print_usage(response)
+
+
+def image(debug):
+    while True:
+        user_input = input('you: ')
+        if user_input.lower() in ['quit', 'exit']:
+            break
+        response = generate_image_response(user_input)
+
+        for i, image in enumerate(response['data']):
+            text = f'image {i+1}'
+            print(f"\x1b]8;;{image['url']}\a{text}\x1b]8;;\a")
+            print(image['url'])
+        if debug:
+            print_usage(response)
+
+
+def print_usage(response):
+    print(response)
+
+
+def main(debug=False):
+    if debug:
+        print('debug mode')
+    input_type = input('chat or image? ')
+    if input_type == 'chat':
+        chat(debug)
+    elif input_type == 'image':
+        image(debug)
+    else:
+        print('invalid input type')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+    main(args.debug)
